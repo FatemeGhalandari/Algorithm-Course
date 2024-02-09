751b19c186c9606137407987a1b3efcb950dd089
FatemeGhalandari <fghalandarii@gmail.com>
2024-02-09 12:39:32 +0330

    Implemented Quick sort algorithm

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,31 @@
-export default function quick_sort(arr: number[]): void {
+function quickSort(arr: number[], lo: number, hi: number): void {
+    //base
+    if (lo >= hi) {
+        return;
+    }
+    const pivot = partition(arr, lo, hi);
 
+    //recursion
+    quickSort(arr, lo, pivot - 1);
+    quickSort(arr, pivot + 1, hi);
+}
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+    }
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+    return idx;
+}
+export default function quick_sort(arr: number[]): void {
+    quickSort(arr, 0, arr.length - 1);
 }
