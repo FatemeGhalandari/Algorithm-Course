8b3a4415a903f2dbbef626ae00fad4aa240a5c79
FatemeGhalandari <fghalandarii@gmail.com>
2024-02-10 23:06:19 +0330

    Implemented Binary Tree Breadth First Search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,17 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: (BinaryNode<Number> | null)[] = [head];
 
+    while (queue.length) {
+        const curr = queue.shift() as BinaryNode<Number> | null | undefined;
+        if (!curr) {
+            continue;
+        }
+        if (curr.value === needle) {
+            return true;
+        }
+
+        queue.push(curr.left);
+        queue.push(curr.right);
+    }
+    return false;
 }
